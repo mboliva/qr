febb9fa8909ee47b767380932c026935eb94099d
Mario Bolivar <mariobolivar22@gmail.com>
2018-07-31 09:30:34 -0500

    1
    1

--- lectorqr/js/lectorQR.js
+++ lectorqr/js/lectorQR.js
@@ -6,7 +6,7 @@
 	);
 	scanner.addListener("scan", function(content){
 		alert("Escaneo de contenido: "+content);			
-		window.open(content, "_blank");
+		window.open("192.168.10.161/qr/consulta.html");
 		
 	});
 	Instascan.Camera.getCameras().then(cameras =>
